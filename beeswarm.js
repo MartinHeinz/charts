2a1dfc5ae7c1b81108e10ab8b74f1d40c8d6d86b
MartinHeinz <martin7.heinz@gmail.com>
2020-06-14 21:30:06 +0200

    Beeswarm chart with WHO data.

--- beeswarm.js
+++ beeswarm.js
@@ -1,82 +1,199 @@
-let height = 240;
-let width = 480;
-let radius = 3;
-let padding = 1.5;
-let margin = ({top: 20, right: 20, bottom: 30, left: 20});
+let height = 300;
+let width = 1000;
+let margin = ({top: 0, right: 40, bottom: 34, left: 40});
 
+let xScale = d3.scaleLinear()
+    .range([margin.left, width - margin.right]);
 
-d3.csv("http://localhost:8000/data/cars-2.csv").then(function (data) {
-    data = data.map(({Name: name, Weight_in_lbs: value}) => ({name, value: +value}));
-    console.log(data);
-    let x = d3.scaleLinear()
-        .domain(d3.extent(data, d => d.value))
-        .range([margin.left, width - margin.right]);
+let xAxis = d3.axisBottom(xScale)
+    .ticks(10, ".0s")
+    .tickSizeOuter(0);
 
-    let xAxis = g => g
-        .attr("transform", `translate(0,${height - margin.bottom})`)
-        .call(d3.axisBottom(x).tickSizeOuter(0));
+var colors = d3.scaleOrdinal()
+    .domain(["asia", "africa", "northAmerica", "europe", "southAmerica", "oceania"])
+    .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33']);
 
-    const svg = d3.select("svg")
-        .attr("viewBox", [0, 0, width, height]);
+d3.select("#africaColor").style("color", colors("africa"));
+d3.select("#namericaColor").style("color", colors("northAmerica"));
+d3.select("#samericaColor").style("color", colors("southAmerica"));
+d3.select("#asiaColor").style("color", colors("asia"));
+d3.select("#europeColor").style("color", colors("europe"));
+d3.select("#oceaniaColor").style("color", colors("oceania"));
+
+let formatNumber = d3.format(",");
+
+let tooltip = d3.select("#svganchor").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
+let svg = d3.select("#svganchor")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+let xLine = svg.append("line")
+    .attr("stroke", "gray")
+    .attr("stroke-dasharray", "1,2");
+
+let chartState = {};
+
+chartState.variable = "total";
+chartState.scale = "scaleLinear";
+chartState.legend = "Total Deaths";
+
+d3.csv("http://localhost:8000/data/who_suicide_stats.csv").then(function (data) {
+
+    let dataSet = data;
+
+    xScale.domain(d3.extent(data, function (d) {
+        return +d.total;
+    }));
 
     svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + (height - margin.bottom) + ")")
         .call(xAxis);
 
-    dodge = (data, radius) => {
-        const radius2 = radius ** 2;
+    let legend = svg.append("text")
+        .attr("class", "legend");
 
-        const circles = data.map(d => ({x: x(d.value), data: d})).sort((a, b) => a.x - b.x);
-        console.log(circles);
-        const epsilon = 1e-3;
-        let head = null, tail = null;
+    redraw(chartState.variable);
 
-        // Returns true if circle ⟨x,y⟩ intersects with any circle in the queue.
-        function intersects(x, y) {
-            let a = head;
-            while (a) {
-                if (radius2 - epsilon > (a.x - x) ** 2 + (a.y - y) ** 2) {
-                    return true;
+    d3.selectAll(".button1").on("click", function(){
+        let thisClicked = this.value;
+        chartState.variable = thisClicked;
+        if (thisClicked === "total") {
+            chartState.legend = "Total Deaths";
+        }
+        if (thisClicked === "perCapita"){
+            chartState.legend = "Per Capita Deaths";
+        }
+        redraw(chartState.variable);
+    });
+
+    d3.selectAll(".button2").on("click", function(){
+        chartState.scale = this.value;
+        redraw(chartState.variable);
+    });
+
+
+    d3.selectAll("input").on("change", filter);
+
+    function redraw(variable){
+
+        if (chartState.scale === "scaleLinear") {
+            xScale = d3.scaleLinear().range([ margin.left, width - margin.right ])
+        }
+
+        if (chartState.scale === "scaleLog") {
+            xScale = d3.scaleLog().range([ margin.left, width - margin.right ]);
+        }
+
+        xScale.domain(d3.extent(dataSet, function(d) {
+            return +d[variable];
+        }));
+
+        let xAxis = d3.axisBottom(xScale)
+            .ticks(10, ".0s")
+            .tickSizeOuter(0);
+
+        d3.transition(svg).select(".x.axis")
+            .transition()
+            .duration(1000)
+            .call(xAxis);
+
+        let simulation = d3.forceSimulation(dataSet)
+            .force("x", d3.forceX(function(d) {
+                return xScale(+d[variable]);
+            }).strength(2))
+            .force("y", d3.forceY((height / 2) - margin.bottom / 2))
+            .force("collide", d3.forceCollide(4))
+            .stop();
+
+        for (let i = 0; i < dataSet.length; ++i) {
+            simulation.tick();
+        }
+
+        let countriesCircles = svg.selectAll(".countries")
+            .data(dataSet, function(d) { return d.country});
+
+        countriesCircles.exit()
+            .transition()
+            .duration(1000)
+            .attr("cx", 0)
+            .attr("cy", (height / 2) - margin.bottom / 2)
+            .remove();
+
+        countriesCircles.enter()
+            .append("circle")
+            .attr("class", "countries")
+            .attr("cx", 0)
+            .attr("cy", (height / 2) - margin.bottom / 2)
+            .attr("r", 3)
+            .attr("fill", function(d){ return colors(d.continent)})
+            .merge(countriesCircles)
+            .transition()
+            .duration(2000)
+            .attr("cx", function(d) { return d.x; })
+            .attr("cy", function(d) { return d.y; });
+
+        legend.text(chartState.legend);
+
+        d3.selectAll(".countries").on("mousemove", function(d) {
+            tooltip.html("Country: <strong>" + d.country + "</strong><br>"
+                + chartState.legend.slice(0, chartState.legend.indexOf(",")) + ": <strong>" + formatNumber(d[variable]) + "</strong>" + chartState.legend.slice(chartState.legend.lastIndexOf(" ")))
+                .style('top', d3.event.pageY - 12 + 'px')
+                .style('left', d3.event.pageX + 25 + 'px')
+                .style("opacity", 0.9);
+
+            xLine.attr("x1", d3.select(this).attr("cx"))
+                .attr("y1", d3.select(this).attr("cy"))
+                .attr("y2", (height - margin.bottom))
+                .attr("x2",  d3.select(this).attr("cx"))
+                .attr("opacity", 1);
+
+        }).on("mouseout", function(d) {
+            tooltip.style("opacity", 0);
+            xLine.attr("opacity", 0);
+        });
+
+        //end of redraw
+    }
+
+    function filter(){
+
+        function getCheckedBoxes(checkboxName) {
+            let checkboxes = document.getElementsByName(checkboxName);
+            let checkboxesChecked = [];
+            for (let i = 0; i < checkboxes.length; i++) {
+                if (checkboxes[i].checked) {
+                    checkboxesChecked.push(checkboxes[i].defaultValue);
                 }
-                a = a.next;
             }
-            return false;
+            return checkboxesChecked.length > 0 ? checkboxesChecked : null;
         }
 
-        // Place each circle sequentially.
-        for (const b of circles) {
+        let checkedBoxes = getCheckedBoxes("continent");
 
-            // Remove circles from the queue that can’t intersect the new circle b.
-            while (head && head.x < b.x - radius2) head = head.next;
+        let newData = [];
 
-            // Choose the minimum non-intersecting tangent.
-            if (intersects(b.x, b.y = 0)) {
-                let a = head;
-                b.y = Infinity;
-                do {
-                    let y = a.y + Math.sqrt(radius2 - (a.x - b.x) ** 2);
-                    if (y < b.y && !intersects(b.x, y)) b.y = y;
-                    a = a.next;
-                } while (a);
-            }
-
-            // Add b to the queue.
-            b.next = null;
-            if (head === null) head = tail = b;
-            else tail = tail.next = b;
+        if (checkedBoxes == null){
+            dataSet = newData;
+            redraw();
+            return;
         }
 
-        return circles;
-    };
+        for (let i = 0; i < checkedBoxes.length; i++){
+            let newArray = data.filter(function(d){
+                return d.continent == checkedBoxes[i];
+            });
+            Array.prototype.push.apply(newData, newArray);
+        }
 
-    svg.append("g")
-        .selectAll("circle")
-        .data(dodge(data, radius * 2 + padding))
-        .join("circle")
-        .attr("cx", d => d.x)
-        .attr("cy", d => height - margin.bottom - radius - padding - d.y)
-        .attr("r", radius)
-        .append("title")
-        .text(d => d.data.name);
+        dataSet = newData;
+        redraw(chartState.variable);
+    }
+
 }).catch(function (error) {
     if (error) throw error;
 });
