26d186686bf41f9a4500799647722143b5650b75
MartinHeinz <martin7.heinz@gmail.com>
2020-06-18 20:13:43 +0200

    Docs for beeswarm chart.

--- beeswarm/beeswarm.js
+++ beeswarm/beeswarm.js
@@ -9,21 +9,25 @@ let xAxis = d3.axisBottom(xScale)
     .ticks(10, ".0s")
     .tickSizeOuter(0);
 
+// Data structure describing chart scales
 let Scales = {
     lin: "scaleLinear",
     log: "scaleLog"
 };
 
+// Data structure describing volume of displayed data
 let Count = {
     total: "total",
     perCap: "perCapita"
 };
 
+// Data structure describing legend fields value
 let Legend = {
     total: "Total Deaths",
     perCap: "Per Capita Deaths"
 };
 
+// Colors used for circles depending on continent
 let colors = d3.scaleOrdinal()
     .domain(["asia", "africa", "northAmerica", "europe", "southAmerica", "oceania"])
     .range(['#D81B60','#1976D2','#388E3C','#FBC02D','#E64A19','#455A64']);
@@ -37,6 +41,7 @@ d3.select("#oceaniaColor").style("color", colors("oceania"));
 
 let formatNumber = d3.format(",");
 
+// Create tooltip div and make it invisible
 let tooltip = d3.select("#svganchor").append("div")
     .attr("class", "tooltip")
     .style("opacity", 0);
@@ -46,6 +51,7 @@ let svg = d3.select("#svganchor")
     .attr("width", width)
     .attr("height", height);
 
+// Create line for X axis
 let xLine = svg.append("line")
     .attr("stroke", "rgb(96,125,139)")
     .attr("stroke-dasharray", "1,2");
@@ -56,10 +62,12 @@ chartState.variable = Count.total;
 chartState.scale = Scales.lin;
 chartState.legend = Legend.total;
 
+// Load and process data
 d3.csv("http://localhost:8000/data/who_suicide_stats.csv").then(function (data) {
 
     let dataSet = data;
 
+    // Set chart domain max value to the highest total value in data set
     xScale.domain(d3.extent(data, function (d) {
         return +d.total;
     }));
@@ -69,6 +77,7 @@ d3.csv("http://localhost:8000/data/who_suicide_stats.csv").then(function (data) 
         .attr("transform", "translate(0," + (height - margin.bottom) + ")")
         .call(xAxis);
 
+    // Insert text into legend (and add styles)
     let legend = svg.append("text")
         .attr("x", width / 2)
         .attr("y", height - 2)
@@ -78,6 +87,7 @@ d3.csv("http://localhost:8000/data/who_suicide_stats.csv").then(function (data) 
 
     redraw(chartState.variable);
 
+    // Listen to click on "total" and "per capita" buttons and trigger redraw when they are clicked
     d3.selectAll(".count").on("click", function() {
         let thisClicked = this.value;
         chartState.variable = thisClicked;
@@ -90,15 +100,19 @@ d3.csv("http://localhost:8000/data/who_suicide_stats.csv").then(function (data) 
         redraw(chartState.variable);
     });
 
+    // Listen to click on "scale" buttons and trigger redraw when they are clicked
     d3.selectAll(".scale").on("click", function() {
         chartState.scale = this.value;
         redraw(chartState.variable);
     });
 
 
+    // Trigger filter function whenever checkbox is ticked/unticked
     d3.selectAll("input").on("change", filter);
 
-    function redraw(variable){
+    function redraw(variable) {
+
+        // Set scale type based on button clicked
         if (chartState.scale === Scales.lin) {
             xScale = d3.scaleLinear().range([ margin.left, width - margin.right ])
         }
@@ -112,7 +126,7 @@ d3.csv("http://localhost:8000/data/who_suicide_stats.csv").then(function (data) 
         }));
 
         let xAxis;
-
+        // Set X axis based on new scale. If chart is set to "per capita" use numbers with one decimal point
         if (chartState.variable === Count.perCap) {
             xAxis = d3.axisBottom(xScale)
                 .ticks(10, ",.1f")
@@ -130,6 +144,7 @@ d3.csv("http://localhost:8000/data/who_suicide_stats.csv").then(function (data) 
             .duration(1000)
             .call(xAxis);
 
+        // Create simulation for resolving collisions
         let simulation = d3.forceSimulation(dataSet)
             .force("x", d3.forceX(function(d) {
                 return xScale(+d[variable]);
@@ -138,10 +153,12 @@ d3.csv("http://localhost:8000/data/who_suicide_stats.csv").then(function (data) 
             .force("collide", d3.forceCollide(9))
             .stop();
 
+        // Manually run simulation
         for (let i = 0; i < dataSet.length; ++i) {
-            simulation.tick();
+            simulation.tick(10);
         }
 
+        // Create country circles
         let countriesCircles = svg.selectAll(".countries")
             .data(dataSet, function(d) { return d.country });
 
@@ -167,9 +184,12 @@ d3.csv("http://localhost:8000/data/who_suicide_stats.csv").then(function (data) 
 
         legend.text(chartState.legend);
 
+        // Show tooltip when hovering over circle (data for respective country)
         d3.selectAll(".countries").on("mousemove", function(d) {
-            tooltip.html("Country: <strong>" + d.country + "</strong><br>"
-                + chartState.legend.slice(0, chartState.legend.indexOf(",")) + ": <strong>" + formatNumber(d[variable]) + "</strong>" + chartState.legend.slice(chartState.legend.lastIndexOf(" ")))
+            tooltip.html(`Country: <strong>${d.country}</strong><br>
+                          ${chartState.legend.slice(0, chartState.legend.indexOf(","))}: 
+                          <strong>${formatNumber(d[variable])}</strong>
+                          ${chartState.legend.slice(chartState.legend.lastIndexOf(" "))}`)
                 .style('top', d3.event.pageY - 12 + 'px')
                 .style('left', d3.event.pageX + 25 + 'px')
                 .style("opacity", 0.9);
@@ -187,6 +207,7 @@ d3.csv("http://localhost:8000/data/who_suicide_stats.csv").then(function (data) 
 
     }
 
+    // Filter data based on which checkboxes are ticked
     function filter() {
 
         function getCheckedBoxes(checkboxName) {
