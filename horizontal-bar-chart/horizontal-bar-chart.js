7f1a49420931f7be7b2bbebdeffc146190f22782
MartinHeinz <martin7.heinz@gmail.com>
2020-07-12 19:54:12 +0200

    Refactoring.

--- horizontal-bar-chart/horizontal-bar-chart.js
+++ horizontal-bar-chart/horizontal-bar-chart.js
@@ -6,11 +6,123 @@ let xScale = d3.scaleLinear()
     .range([margin.left, width - margin.right]);
 
 let xMin = 0;
+let rectSize = 100;
+let yScale;
+let yAxis;
+let bars;
+let colors;
+let gradient;
+let colorRect;
+let legend;
+let minLegend;
+let maxLegend;
 
-let yScale = d3.scaleBand()
-    .range([margin.top, height - margin.bottom])
-    .paddingInner(0.4)
-    .paddingOuter(0.4);
+function performYAxisTransition() {
+
+    svg.transition(svg).select(".y.axis")
+        .transition()
+        .duration(1000)
+        .call(yAxis);
+}
+
+function performBarsTransition() {
+
+    bars.transition()
+        .duration(1000)
+        .attr("x", function(d) { return xScale(d.start); })
+        .attr("y", function(d) { return yScale(d.title); })
+        .attr("width", function(d) { return xScale(d.end) - xScale(d.start);})
+        .attr("height", yScale.bandwidth())
+        .attr("rx", 4)
+        .attr("ry", 4)
+        .attr("fill", function(d) {
+            return colors(d.start - d.end);
+        });
+}
+
+function createBars(ds) {
+    bars = svg.selectAll(".bars")
+        .data(ds)
+        .enter()
+        .append("rect");
+}
+
+function createLegend() {
+    gradient = svg.append("defs")
+        .append("linearGradient")
+        .attr("id", "gradient")
+        .attr("x1", "0%")
+        .attr("y1", "0%")
+        .attr("x2", "100%")
+        .attr("y2", "0%")
+        .attr("spreadMethod", "pad");
+
+    gradient.append("stop")
+        .attr("offset", "0%")
+        .attr("stop-color", "#2f4b7c")
+        .attr("stop-opacity", 1);
+
+    gradient.append("stop")
+        .attr("offset", "33%")
+        .attr("stop-color", "#a05195")
+        .attr("stop-opacity", 1);
+
+    gradient.append("stop")
+        .attr("offset", "75%")
+        .attr("stop-color", "#f95d6a")
+        .attr("stop-opacity", 1);
+
+    gradient.append("stop")
+        .attr("offset", "100%")
+        .attr("stop-color", "#ffa600")
+        .attr("stop-opacity", 1);
+
+    colorRect = svg.append("rect")
+        .attr("x", ((margin.left + (width - margin.right)) / 2) - rectSize)
+        .attr("y", height - 20)
+        .attr("width", rectSize * 2)
+        .attr("height", 8)
+        .attr("rx", 4)
+        .attr("ry", 4)
+        .style("fill", "url(#gradient)");
+
+    legend = svg.append("text")
+        .attr("text-anchor", "middle")
+        .attr("x", (margin.left + (width - margin.right)) / 2)
+        .attr("y", height - 24)
+        .attr("class", "legend")
+        .text("color legend");
+
+    minLegend = svg.append("text")
+        .attr("text-anchor", "end")
+        .attr("x", ((margin.left + (width - margin.right)) / 2) - rectSize - 4)
+        .attr("y", height - 13)
+        .attr("class", "legend")
+        .text("Minimum");
+
+    maxLegend = svg.append("text")
+        .attr("text-anchor", "start")
+        .attr("x", ((margin.left + (width - margin.right)) / 2) + rectSize + 4)
+        .attr("y", height - 13)
+        .attr("class", "legend")
+        .text("Maximum");
+}
+
+function updateYScale() {
+    yScale = d3.scaleBand()
+        .range([margin.top, height - margin.bottom])
+        .paddingInner(0.4)
+        .paddingOuter(0.4);
+}
+
+function updateYAxis() {
+    yAxis = d3.axisLeft(yScale)
+        .tickSizeOuter(0)
+        .tickSizeInner(0);
+}
+
+updateYScale();
+updateYAxis();
 
 let xAxis = d3.axisBottom(xScale)
     .tickSizeInner(-(height - margin.top - margin.bottom))
@@ -18,13 +130,7 @@ let xAxis = d3.axisBottom(xScale)
     .tickSizeOuter(0)
     .tickPadding(6);
 
-let yAxis = d3.axisLeft(yScale)
-    .tickSizeOuter(0)
-    .tickSizeInner(0);
-
-let formatNumber = d3.format(".2f");
-
-let tt = d3.select("#svganchor").append("div")
+let tooltip = d3.select("#svganchor").append("div")
     .attr("class", "tooltip")
     .style("opacity", 0);
 
@@ -36,18 +142,11 @@ let svg = d3.select("#svganchor")
 let inputFrom = d3.select("#from");
 let inputTo = d3.select("#to");
 
-// let chartTitle = svg.append("text")
-//     .attr("class", "chartTitle")
-//     .attr("text-anchor", "middle")
-//     .attr("y", 16)
-//     .attr("x", (margin.left + (width - margin.right))/2)
-//     .text("Epidemics");
-
 let xTitle = svg.append("text")
     .attr("class", "axisTitle")
     .attr("text-anchor", "middle")
     .attr("y", (height - margin.bottom) + 34)
-    .attr("x", (margin.left + (width - margin.right))/2)
+    .attr("x", (margin.left + (width - margin.right)) / 2)
     .text("Time (Years)");
 
 function range(start, end, increment) {
@@ -69,6 +168,7 @@ function range(start, end, increment) {
     return array;
 }
 
+// d3.csv("https://martinheinz.github.io/charts/data/epidemics.csv").then(function(data) {
 d3.csv("../data/epidemics.csv").then(function(data) {
 
     data.forEach(function(d) {
@@ -120,126 +220,29 @@ d3.csv("../data/epidemics.csv").then(function(data) {
     let colorMax = d3.min(dataSet, function(d) {
         return d.start - d.end;
     });
-    let colorRange = range(colorMin, colorMax, (colorMax/4)).map(x => Math.round(x)).reverse();
-    let colors = d3.scaleLinear()
+    let colorRange = [-90, -75, -45, -25, -8, -5, 0]; // range(colorMin, colorMax, (colorMax/6)).map(x => Math.round(x)).reverse();
+    console.log(colorRange);
+    colors = d3.scaleLinear()
         .domain(colorRange)
-        .range(["#ffa600", "#f95d6a", "#a05195", "#2f4b7c"]);
+        .range(["#ffa600", "#ff7c43", "#f95d6a", "#d45087", "#a05195", "#003f5c"]);
 
-    let bars = svg.selectAll(".myBars")
-        .data(dataSet)
-        .enter()
-        .append("rect");
+    createBars(dataSet);
 
-    bars.attr("x", function(d) { return xScale(d.start);})
-        .attr("y", function(d) { return yScale(d.title);})
-        .attr("width", function(d) {
-            return xScale(d.end) - xScale(d.start);})
-        .attr("height", yScale.bandwidth())
-        .attr("rx", 4)
-        .attr("ry", 4)
-        .attr("fill", function(d) {
-            return colors(d.start - d.end);
-        });
+    performBarsTransition();
 
     let gX = svg.append("g")
         .attr("class", "x axis")
-        .attr("transform", "translate(0," + (height - margin.bottom) + ")")
+        .attr("transform", `translate(0,${height - margin.bottom})`)
         .call(xAxis);
 
     let gY = svg.append("g")
         .attr("class", "y axis")
-        .attr("transform", "translate(" + margin.left + ",0)")
+        .attr("transform", `translate(${margin.left},0)`)
         .call(yAxis);
 
     gX.lower();
 
-    d3.selectAll(".y.axis text").on("mousemove", function(d) {
-        let filteredData = dataSet.filter(function(e) { return e.title === d})[0];
-        tt.html(`Name: <strong>${filteredData.title}</strong><br>
-                 Location: <strong>${filteredData.location}</strong>
-                `)
-            .style('top', d3.event.pageY - 12 + 'px')
-            .style('left', d3.event.pageX + 15 + 'px')
-            .style("opacity", 1);
-    }).on("mouseout", function() {
-        tt.style("opacity", 0)
-    });
-
-    let gradient = svg.append("defs")
-        .append("linearGradient")
-        .attr("id", "gradient")
-        .attr("x1", "0%")
-        .attr("y1", "0%")
-        .attr("x2", "100%")
-        .attr("y2", "0%")
-        .attr("spreadMethod", "pad");
-
-    gradient.append("stop")
-        .attr("offset", "0%")
-        .attr("stop-color", "#2f4b7c")
-        .attr("stop-opacity", 1);
-
-    gradient.append("stop")
-        .attr("offset", "33%")
-        .attr("stop-color", "#a05195")
-        .attr("stop-opacity", 1);
-
-    gradient.append("stop")
-        .attr("offset", "75%")
-        .attr("stop-color", "#f95d6a")
-        .attr("stop-opacity", 1);
-
-    gradient.append("stop")
-        .attr("offset", "100%")
-        .attr("stop-color", "#ffa600")
-        .attr("stop-opacity", 1);
-
-    let rectSize = 100;
-
-    let colorRect = svg.append("rect")
-        .attr("x", ((margin.left + (width - margin.right)) / 2) - rectSize)
-        .attr("y", height - 20)
-        .attr("width", rectSize * 2)
-        .attr("height", 8)
-        .attr("rx", 4)
-        .attr("ry", 4)
-        .style("fill", "url(#gradient)");
-
-    let legend = svg.append("text")
-        .attr("text-anchor", "middle")
-        .attr("x", (margin.left + (width - margin.right)) / 2)
-        .attr("y", height - 24)
-        .attr("class", "legend")
-        .text("color legend");
-
-    let minLegend = svg.append("text")
-        .attr("text-anchor", "end")
-        .attr("x", ((margin.left + (width - margin.right))/2) - rectSize - 4)
-        .attr("y", height - 13)
-        .attr("class", "legend")
-        .text("Minimum");
-
-    let maxLegend = svg.append("text")
-        .attr("text-anchor", "start")
-        .attr("x", ((margin.left + (width - margin.right))/2) + rectSize + 4)
-        .attr("y", height - 13)
-        .attr("class", "legend")
-        .text("Maximum");
-
-    bars.on("mousemove", function(d) {
-        bars.attr("opacity", 0.25);
-        d3.select(this).attr("opacity", 1);
-        tt.html(`Name: <strong>${d.title}</strong><br>
-                 Time Span: <strong>${d.span}</strong><br>
-                 Death Toll: <strong>${d.toll === 0 ? "Unknown" : d.toll}</strong>
-                `)
-            .style('top', d3.event.pageY + 16 + 'px')
-            .style('left', d3.event.pageX - 30 + 'px')
-            .style("opacity", 1);
-    }).on("mouseout", function() {
-        tt.style("opacity", 0);
-        bars.attr("opacity", 1);
-    });
+    createLegend();
 
     d3.selectAll(".sort").on("click", function() {
         let thisClicked = this.value;
@@ -251,22 +254,51 @@ d3.csv("../data/epidemics.csv").then(function(data) {
         drawSince(thisClicked);
     });
 
+    createListeners();
+
+    function createListeners() {
+        d3.selectAll(".y.axis text").on("mousemove", function(d) {
+            let filteredData = dataSet.filter(function(e) { return e.title === d})[0];
+            tooltip.html(`Name: <strong>${filteredData.title}</strong><br>
+                 Location: <strong>${filteredData.location}</strong>
+                `)
+                .style('top', d3.event.pageY - 12 + 'px')
+                .style('left', d3.event.pageX + 15 + 'px')
+                .style("opacity", 1);
+        }).on("mouseout", function() {
+            tooltip.style("opacity", 0)
+        });
+
+        bars.on("mousemove", function(d) {
+            bars.attr("opacity", 0.25);
+            d3.select(this).attr("opacity", 1);
+            tooltip.html(`Name: <strong>${d.title}</strong><br>
+                 Time Span: <strong>${d.span}</strong><br>
+                 Death Toll: <strong>${d.toll === 0 ? "Unknown" : d.toll}</strong>
+                `)
+                .style('top', d3.event.pageY + 16 + 'px')
+                .style('left', d3.event.pageX - 30 + 'px')
+                .style("opacity", 1);
+        }).on("mouseout", function() {
+            tooltip.style("opacity", 0);
+            bars.attr("opacity", 1);
+        });
+    }
+
     function drawSort(sort) {
 
-        let sortedData;
-
         if(sort === "sortTotalDeathToll") {
-            sortedData = dataSet.sort(function(a, b) {
+            dataSet.sort(function(a, b) {
                 return d3.descending(a.toll , b.toll);
             });
         }
         else if(sort === "sortStartYear") {
-            sortedData = dataSet.sort(function(a, b) {
+            dataSet.sort(function(a, b) {
                 return d3.ascending(a.start , b.start);
             });
         }
         else if(sort === "sortTotalSpan") {
-            sortedData = dataSet.sort(function(a, b) {
+            dataSet.sort(function(a, b) {
                 return d3.descending(a.span , b.span);
             });
         }
@@ -277,11 +309,7 @@ d3.csv("../data/epidemics.csv").then(function(data) {
             .duration(1000)
             .attr("y", function(d) { return yScale(d.title); });
 
-        svg.transition(svg).select(".y.axis")
-            .transition()
-            .duration(1000)
-            .call(yAxis);
-
+        performYAxisTransition();
     }
 
     function drawSince(since) {
@@ -313,98 +341,49 @@ d3.csv("../data/epidemics.csv").then(function(data) {
 
         bars.data([]).exit().remove();  // Remove all rows
 
-        bars = svg.selectAll(".myBars")  // Add all currently selected rows back in
-            .data(dataSet)
-            .enter()
-            .append("rect");
+        createBars(dataSet);  // Add all currently selected rows back in
 
         height = Math.ceil(dataSet.length*12);
 
-        yScale = d3.scaleBand()
-            .range([margin.top, height - margin.bottom])
-            .paddingInner(0.4)
-            .paddingOuter(0.4);
-
-        yAxis = d3.axisLeft(yScale)
-            .tickSizeOuter(0)
-            .tickSizeInner(0);
+        updateYScale();
+        updateYAxis();
 
         yScale.domain(dataSet.map(function(d) { return d.title; }));
 
-        bars.transition()
-            .duration(1000)
-            .attr("x", function(d) { return xScale(d.start); })
-            .attr("y", function(d) { return yScale(d.title); })
-            .attr("width", function(d) { return xScale(d.end) - xScale(d.start);})
-            .attr("height", yScale.bandwidth())
-            .attr("rx", 4)
-            .attr("ry", 4)
-            .attr("fill", function(d) {
-                return colors(d.start - d.end);
-            });
+        performBarsTransition();
 
-        svg.transition(svg).select(".x.axis").transition().duration(1000)
-            .call(xAxis);
-
-        svg.transition(svg).select(".y.axis")
+        svg.transition(svg).select(".x.axis")
             .transition()
             .duration(1000)
-            .call(yAxis);
+            .call(xAxis);
 
-        d3.selectAll(".y.axis text").on("mousemove", function(d) {
-            let filteredData = dataSet.filter(function(e) { return e.title === d})[0];
-            tt.html(`Name: <strong>${filteredData.title}</strong><br>
-                 Location: <strong>${filteredData.location}</strong>
-                `)
-                .style('top', d3.event.pageY - 12 + 'px')
-                .style('left', d3.event.pageX + 15 + 'px')
-                .style("opacity", 1);
-        }).on("mouseout", function() {
-            tt.style("opacity", 0)
-        });
+        performYAxisTransition();
 
-        bars.on("mousemove", function(d) {
-            bars.attr("opacity", 0.25);
-            d3.select(this).attr("opacity", 1);
-            tt.html(`Name: <strong>${d.title}</strong><br>
-                 Time Span: <strong>${d.span}</strong><br>
-                 Death Toll: <strong>${d.toll === 0 ? "Unknown" : d.toll}</strong>
-                `)
-                .style('top', d3.event.pageY + 16 + 'px')
-                .style('left', d3.event.pageX - 30 + 'px')
-                .style("opacity", 1);
-        }).on("mouseout", function() {
-            tt.style("opacity", 0);
-            bars.attr("opacity", 1);
-        });
+        createListeners();
 
         svg.attr("height", height);
         d3.select("#svganchor")
-            .attr("height", height);
+          .attr("height", height);
 
         d3.axisBottom(xScale)
-            .tickSizeInner(-(height - margin.top - margin.bottom));
+          .tickSizeInner(-(height - margin.top - margin.bottom));
 
         xTitle.attr("y", (height - margin.bottom) + 34);
 
         gX.attr("transform", "translate(0," + (height - margin.bottom) + ")");
 
         legend.attr("x", (margin.left + (width - margin.right)) / 2)
-            .attr("y", height - 24);
+              .attr("y", height - 24);
 
 
         minLegend.attr("y", height - 13);
 
         maxLegend.attr("x", ((margin.left + (width - margin.right))/2) + rectSize + 4)
-            .attr("y", height - 13);
+                 .attr("y", height - 13);
 
-        colorRect
-            .attr("x", ((margin.left + (width - margin.right)) / 2) - rectSize)
-            .attr("y", height - 20);
-
+        colorRect.attr("x", ((margin.left + (width - margin.right)) / 2) - rectSize)
+                 .attr("y", height - 20);
     }
-
-
 }).catch(function (error) {
     if (error) throw error;
 });
